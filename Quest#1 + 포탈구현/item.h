4a5db27327cbbf6dbc639bb0f346d749610dce55
rhtmdgus <163988735+rhtmdgus@users.noreply.github.com>
2024-11-18 14:39:03 +0900

    아이템 종류 파일 추가

---
+++ Quest#1 + 포탈구현/item.h
@@ -0,0 +1,26 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+
+typedef struct
+{
+    char name[50];
+    int attackpoint;
+    int defensepoint;
+
+} weapon;
+
+
+
+typedef struct
+{
+    char name[50];
+    int attackpoint;
+    int defensepoint;
+
+} equipment;
+
+
+
+
+#endif // ITEM_H
