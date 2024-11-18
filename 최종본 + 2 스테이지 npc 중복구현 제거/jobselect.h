dc25a76cf89af78f2ca3efd4daddf5d4e5bc7245
HyukJaeKwon2 <gurwo5333@gmail.com>
2024-11-18 22:36:31 +0900

    Quest #3

---
+++ 최종본 + 2 스테이지 npc 중복구현 제거/jobselect.h
@@ -0,0 +1,9 @@
+#ifndef JOBSELECT_H
+#define JOBSELECT_H
+
+#include "utility.h"
+
+void jobSelect_screen();
+void jobSelect();
+
+#endif // JOBSELECT_H
---
+++ 최종본 + 2 스테이지 npc 중복구현 제거/npc.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "npc.h"
+
+NpcData npcList[MAX_NPC];		 //배열 정의
+NpcData* currentNPC = NULL;       // 포인터 정의
+
+void initializeNpc()
+{
+	npcList[0] = { "장 돌쇠", true, 0, {5, 3}, Nobody };
+	npcList[1] = { "김두환", true, 0, {7, 3}, VolunArmy };
+	npcList[2] = { "조병옥", true, 0, {9, 3}, GoverArmy };
+	npcList[3] = { "나까무라", true, 0, {11, 3}, JapArmy };
+	npcList[4] = { "김 아무개", true, 0, {13, 3}, Nobody2 };
+}
+
+void drawNpc(NpcData* npcList)
+{
+	for (int i = 0; i < MAX_NPC; i++)
+	{
+		if(player.currentmap == (npcList + i)->currentmap)
+		{
+			setCursorPosition((npcList + i)->pos.x, (npcList + i)->pos.y);
+			if ((npcList + i)->type == Nobody)
+				setColor(7);
+			else if ((npcList + i)->type == GoverArmy)
+				setColor(2);
+			else if ((npcList + i)->type == JapArmy)
+				setColor(4);
+			else if ((npcList + i)->type == VolunArmy)
+				setColor(3);
+			printf("N");
+			setColor(7);
+		}
+	}
+}
