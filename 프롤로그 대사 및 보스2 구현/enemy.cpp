e2cb8bf266dd79b265a17a38a56b1ba17fa42948
youngpol63 <youngpol63@gmail.com>
2024-12-09 18:50:18 +0900

    프롤로그 대사 변경&보스2구현

--- 프롤로그 대사 및 보스2 구현/enemy.cpp
+++ 프롤로그 대사 및 보스2 구현/enemy.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <time.h>
 
-Enemy enemyTemplates[9] = {
+Enemy enemyTemplates[12] = {
     {"왜군 잡졸", 10, 8, 3, {0, 0}, warrior},
     {"왜군 창병", 12, 10, 4, {0, 0}, spear},
     {"왜군 궁병", 8, 6, 2, {0, 0}, archor},
@@ -13,7 +13,10 @@ Enemy enemyTemplates[9] = {
     {"관군 장군", 18, 12, 10, {0, 0}, warrior},
     {"왜군 정예 장수", 40, 25, 15, {0, 0}, warrior},
     {"의병 정예 장군", 30, 30, 12, {0, 0}, warrior},
-    {"관군 정예 장군", 35, 25, 20, {0, 0}, warrior}
+    {"관군 정예 장군", 35, 25, 20, {0, 0}, warrior},
+    {"왜군 정예 장수", 40, 25, 15, {0, 0}, archor},
+    {"의병 정예 장군", 30, 30, 12, {0, 0}, archor},
+    {"관군 정예 장군", 35, 25, 20, {0, 0}, archor},
 };
 
 Enemy currentEnemies[MAX_ENEMY];  // 배열 정의
@@ -112,19 +115,19 @@ void spawnBoss2() {
     pos.y = 24;
     if (player.JRelationship < 0)
     {
-        currentBosses[0] = createBoss(&enemyTemplates[8], pos);
+        currentBosses[0] = createBoss(&enemyTemplates[12], pos);
         drawBoss(&currentBosses[0]);
         map[currentBosses[0].pos.y][currentBosses[0].pos.x] = 'B';
     }
     else if (player.WRelationship < 0)
     {
-        currentBosses[0] = createBoss(&enemyTemplates[7], pos);
+        currentBosses[0] = createBoss(&enemyTemplates[11], pos);
         drawBoss(&currentBosses[0]);
         map[currentBosses[0].pos.y][currentBosses[0].pos.x] = 'B';
     }
     else
     {
-        currentBosses[0] = createBoss(&enemyTemplates[6], pos);
+        currentBosses[0] = createBoss(&enemyTemplates[10], pos);
         drawBoss(&currentBosses[0]);
         map[currentBosses[0].pos.y][currentBosses[0].pos.x] = 'B';
     }
