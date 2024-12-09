6604803789a2bd402e7f9100882c2ff03af012ab
rhtmdgus <163988735+rhtmdgus@users.noreply.github.com>
2024-12-09 13:43:40 +0900

    보스 스킬 구현

--- 12_09 보스 스킬 구현/selectmap.h
+++ 12_09 보스 스킬 구현/selectmap.h
@@ -6,6 +6,12 @@
 
 void map1_0(); // map1_1 함수 선언
 void map1_1(); // map1_2 함수 선언
+void map1_2();
+void map1_3();
+void map1_4();
+void map1_5();
+
+void map1_10(); // map1_10 함수 선언
 
 #endif // SELECTMAP_H
 
